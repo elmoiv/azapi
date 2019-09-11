d31a754dc480a1fc411e27acde4dcbe919b3bf04
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-09-11 17:13:24 +0200

    Update __init__.py

--- azapi/__init__.py
+++ azapi/__init__.py
@@ -2,7 +2,7 @@ from .azapi import AZlyrics
 
 __name__ = 'azapi.azapi.AZlyrics'
 __author__ = 'Khaled H. El-Morshedy'
-__url__ = 'https://github.com/elmoiv/AZLyricsAPI'
+__url__ = 'https://github.com/elmoiv/azapi'
 __description__ = 'A fast and secure AZLyrics API'
 __license__ = 'MIT'
-__version__ = '1.0'
+__version__ = '2.0'
