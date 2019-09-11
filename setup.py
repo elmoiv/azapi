327e61db4119748119798a72561a2f5abecaed8f
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-09-11 17:13:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="azapi",
-    version="1.0.1",
+    version="2.0",
     author="elmoiv",
     author_email="elmoiv@yahoo.com",
     description="Fast and Secure AZLyrics API",
