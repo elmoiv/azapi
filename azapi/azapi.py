3fad6e04dce9470346f91505fd1b75cb817bca4d
Francis Windows-PC <francisgian_fgm100@outlook.com>
2021-04-12 18:24:43 +0800

    added a new parameters to commands
    * getLyrics added title and artist as a parameter (defaulted to None) so they can still set it like z.title = 'bad blood'
    
    * getSongs added artist as a parameter (defaulted to None) so they can still set it like z.artist = 'Taylor Swift'

--- azapi/azapi.py
+++ azapi/azapi.py
@@ -1,6 +1,7 @@
 from .requester import Requester
 from .tools import *
 
+
 class AZlyrics(Requester):
     """
     Fast and Secure API for AZLyrics.com
@@ -13,39 +14,44 @@ class AZlyrics(Requester):
         accuracy (float): used to determine accuracy via jaro algorithm
         proxies (dict): if you want to use proxy while connecting to AZLyrics.com
     """
-    
+
     def __init__(self, search_engine='', accuracy=0.6, proxies={}):
         self.title = ''
         self.artist = ''
         self.search_engine = search_engine
-        
+
         self.accuracy = accuracy
         if not 0 < accuracy <= 1:
             self.accuracy = 0.6
-        
+
         self.proxies = proxies
 
         self.lyrics_history = []
         self.lyrics = ''
         self.songs = {}
 
-    def getLyrics(self, url=None, ext='txt', save=False, path='', sleep=3):
+    def getLyrics(self, title=None, artist=None, url=None, ext='txt', save=False, path='', sleep=3):
         """
         Reterive Lyrics for a given song details
-        
-        Parameters: 
-            url (str): url of the song's Azlyrics page. 
+
+        Parameters:
+            url (str): url of the song's Azlyrics page.
             ext (str): extension of the lyrics saved file, default is ".txt".
             save (bool): allow to or not to save lyrics in a file.
-            sleep (float): cooldown before next request.  
-        
+            sleep (float): cooldown before next request.
+
         Returns:
             lyrics (str): Lyrics of the detected song
         """
+        if title:
+            setattr(self, 'title', title)
+
+            if artist:
+                setattr(self, 'artist', artist)
 
         if not self.artist + self.title:
             raise ValueError("Both artist and title can't be empty!")
-        
+
         # Best cooldown is 5 sec
         time.sleep(sleep)
 
@@ -55,7 +61,7 @@ class AZlyrics(Requester):
             if self.search_engine:
                 # If user can't remember the artist,
                 # he can search by title only
-                
+
                 # Get AZlyrics url via Google Search
                 link = GoogleGet(
                             self.search_engine,
@@ -81,7 +87,7 @@ class AZlyrics(Requester):
 
         # Getting Basic metadata from azlyrics
         metadata = [elm.text for elm in htmlFindAll(page)('b')]
-        
+
         # v3.0.4: Update title and artist attributes with exact names
         self.artist = filtr(metadata[0][:-7], True)
         self.title = filtr(metadata[1][1:-1], True)
@@ -101,10 +107,10 @@ class AZlyrics(Requester):
                                                 ext
                                                 )
                                 )
-                
+
                 with open(p, 'w', encoding='utf-8') as f:
                     f.write(lyrics.strip())
-            
+
             # Store lyrics for later usage
             self.lyrics_history.append(self.lyrics)
             return self.lyrics
@@ -112,23 +118,26 @@ class AZlyrics(Requester):
         self.lyrics = 'No lyrics found :('
         return 2
 
-    def getSongs(self, sleep=3):
+    def getSongs(self, artist=None, sleep=3):
         """
         Reterive a dictionary of songs with their links
 
         Parameters:
-            sleep (float): cooldown before next request.  
-        
+            sleep (float): cooldown before next request.
+
         Returns:
             dict: dictionary of songs with their links
         """
 
+        if artist:
+            setattr(self, 'artist', artist)
+
         if not self.artist:
             raise Exception("Artist can't be empty!")
-        
+
         # Best cooldown is 5 sec
         time.sleep(sleep)
-        
+
         if self.search_engine:
             link = GoogleGet(
                         self.search_engine,
@@ -144,12 +153,12 @@ class AZlyrics(Requester):
                         self.artist,
                         '',
                         1)
-        
+
         albums_page = self.get(link)
         if albums_page.status_code != 200:
             print('Error 404!')
             return {}
-        
+
         # Store songs for later usage
         self.songs = ParseSongs(albums_page)
         return self.songs
