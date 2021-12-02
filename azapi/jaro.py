363b5769b2ee636df67fc078d3bc403b5fcfb4bd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-02 17:20:45 -0500

    :recycle: Update Imports, uniform code style, simplified returns

--- azapi/jaro.py
+++ azapi/jaro.py
@@ -2,39 +2,39 @@
 # Source: https://www.geeksforgeeks.org/jaro-and-jaro-winkler-similarity/
 # This code is contributed by mohit kumar 29 (GeeksforGeeks.com)
 from math import floor
-  
-def jaro_distance(s1, s2): 
-    if (s1 == s2): 
+
+
+def jaro_distance(s1, s2):
+    if s1 == s2:
         return 1.0
-  
+
     len1, len2 = len(s1), len(s2)
     max_dist = floor(max(len1, len2) / 2) - 1
     match = 0
     hash_s1, hash_s2 = [0] * len(s1), [0] * len(s2)
-  
+
     for i in range(len1):
-        for j in range(max(0, i - max_dist),  
-                       min(len2, i + max_dist + 1)):
-            if (s1[i] == s2[j] and hash_s2[j] == 0):
+        for j in range(max(0, i - max_dist), min(len2, i + max_dist + 1)):
+            if s1[i] == s2[j] and hash_s2[j] == 0:
                 hash_s1[i], hash_s2[j] = 1, 1
                 match += 1
                 break
 
-    if (match == 0): 
+    if match == 0:
         return 0.0
 
     t = 0
     point = 0
-  
-    for i in range(len1): 
-        if (hash_s1[i]): 
-            while (hash_s2[point] == 0): 
+
+    for i in range(len1):
+        if hash_s1[i]:
+            while hash_s2[point] == 0:
                 point += 1
-  
-            if (s1[i] != s2[point]): 
+
+            if s1[i] != s2[point]:
                 point += 1
                 t += 1
-    t = t//2
+    t = t // 2
 
-    return (match/ len1 + match / len2 + 
-            (match - t + 1) / match)/ 3.0
+    return (match / len1 + match / len2 + (match - t + 1) / match) / 3.0
+
--- azapi/requester.py
+++ azapi/requester.py
@@ -1,6 +1,8 @@
-import requests, random
+import random
 
-userAgents = '''Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36
+import requests
+
+userAgents = """Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36
 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36
 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.11 Safari/535.19
 Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11
@@ -39,14 +41,24 @@ Mozilla/5.0 (Macintosh; U; Mac OS X 10_6_1; en-US) AppleWebKit/530.5 (KHTML, lik
 Mozilla/5.0 (Macintosh; U; Mac OS X 10_5_7; en-US) AppleWebKit/530.5 (KHTML, like Gecko) Chrome/ Safari/530.5
 Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-US) AppleWebKit/530.9 (KHTML, like Gecko) Chrome/ Safari/530.9
 Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-US) AppleWebKit/530.6 (KHTML, like Gecko) Chrome/ Safari/530.6
-Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-US) AppleWebKit/530.5 (KHTML, like Gecko) Chrome/ Safari/530.5'''
+Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-US) AppleWebKit/530.5 (KHTML, like Gecko) Chrome/ Safari/530.5"""
 
-class Requester():
-    USER_AGENTS = userAgents.split('\n')
-    
+
+class Requester:
+    USER_AGENTS = userAgents.split("\n")
+
     # Inspired from: https://github.com/brianchesley/Lyrics/blob/master/lyrics_data_scrape.py
     def get(self, url, _proxies={}):
-        return requests.get(url, headers={'User-Agent': random.choice(self.USER_AGENTS)}, proxies=_proxies)
-    
+        return requests.get(
+            url,
+            headers={"User-Agent": random.choice(self.USER_AGENTS)},
+            proxies=_proxies,
+        )
+
     def head(self, url, _proxies={}):
-        return requests.head(url, headers={'User-Agent': random.choice(self.USER_AGENTS)}, proxies=_proxies)
+        return requests.head(
+            url,
+            headers={"User-Agent": random.choice(self.USER_AGENTS)},
+            proxies=_proxies,
+        )
+
